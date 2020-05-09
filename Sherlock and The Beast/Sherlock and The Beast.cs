dffe977867c8fcfa8e7e87023525825128ab6435
NDTPine <nguyenducthong1593@gmail.com>
2020-05-09 12:22:40 +0700

    Create Sherlock and The Beast.cs
    Complete.

---
+++ Sherlock and The Beast/Sherlock and The Beast.cs
@@ -0,0 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the decentNumber function below.
+    static void decentNumber(int n) {
+
+
+    }
+
+    static void Main(string[] args) {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            decentNumber(n);
+        }
+    }
+}
