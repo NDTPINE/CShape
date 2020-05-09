a247ed9f4a5c6b9db38af52d2e9e499f39bba8d2
NDTPine <nguyenducthong1593@gmail.com>
2020-05-09 14:21:37 +0700

    Update Sherlock and The Beast.cs
    Complete.

--- Sherlock and The Beast/Sherlock and The Beast.cs
+++ Sherlock and The Beast/Sherlock and The Beast.cs
@@ -16,7 +16,31 @@ class Solution {
 
     // Complete the decentNumber function below.
     static void decentNumber(int n) {
-
+        if (n == 1 || n == 2) Console.WriteLine("-1");
+        else {
+            int numberThree = 1;
+            int numberFive = 1;
+            StringBuilder s1 = new StringBuilder();
+            int intNumber = (int)n/5;
+            for (int i = 0; i <= intNumber;i++){
+                numberFive = n - (i*5);
+                if (numberFive % 3 == 0){
+                    numberThree = i*5;
+                    break;
+                } 
+            }
+            if (numberFive % 3 != 0 && numberThree % 5 != 0) {
+                Console.WriteLine("-1");
+            } else{
+                for (int j = 0; j < numberFive; j++){
+                    s1 = s1.Append("5");
+                }
+                for (int m = 0; m < n - numberFive; m++){
+                    s1 = s1.Append("3");
+                }
+                Console.WriteLine("{0}", s1);
+            }
+        }
 
     }
 
