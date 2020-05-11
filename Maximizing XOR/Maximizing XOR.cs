0775008fc95dfae6d7b5a030792c46c6df2206f4
NDTPine <nguyenducthong1593@gmail.com>
2020-05-11 11:01:01 +0700

    Update Maximizing XOR.cs
    /* Use MSF to resolve the problem*/

--- Maximizing XOR/Maximizing XOR.cs
+++ Maximizing XOR/Maximizing XOR.cs
@@ -16,20 +16,59 @@ class Solution {
 
     // Complete the maximizingXor function below.
     static int maximizingXor(int l, int r) {
+        /* Use MSF to resolve the problem*/
+        int value = l ^ r;
+        int result = 1;
+        while (value >0) {
+            value = value >> 1;
+            result = result << 1;
+        }
+        return result - 1;
+
+        /* TIME OUT 4 test case
         int result;
-        int[] temp = new int[l*r];
         int index = 0;
-        for (int i = 0; i < l; i++){
-            for (int j = 0; j < r; j++){
-                byte[] value1 = BitConverter.GetBytes(i);
-                byte[] value2 = BitConverter.GetBytes(j);
-                int check = BitConverter.ToInt32(value1,0);
+        int max = 0;
+        for (int q = l; q <= r; q++){
+            int[] bin1 = convertToDecToBin(q);
+            for (int p = l; p <= r; p++){
+                int[] bin2 = convertToDecToBin(p);
+                int[] bin3 = bin2;
+                for(int m = 0; m < 10; m++){
+                    bin3[m] = bin1[m] ^ bin2[m];
+                }
+                max = Math.Max(max,convertToBinToDec(bin3));
             }
         }
-        result = check;
-        return result;
-
+        return result = max;*/
     }
+    public static int[] convertToDecToBin(int number){
+        int[] arr = new int[10];       
+        for(int i = 0; number > 0; i++){      
+            arr[i] = number % 2;      
+            number= number/2;    
+        }
+        Array.Reverse(arr);
+        return arr;
+    }
+    public static int convertToBinToDec(int[] arr){
+        double dec = 0;
+        int i = 0;
+        int d;
+        string temp = "";
+        for (int j = 0; j < arr.Length; j++){
+            temp = string.Concat(temp,arr[j].ToString());
+        }
+        int number = Convert.ToInt32(temp);
+        while (number != 0){
+            d = number % 10;
+            dec = dec + d * (int)Math.Pow(2, i); // su dung ham trong C#
+            number = number / 10;
+            i++;
+        }
+        return (int)dec;
+    }
+
 
     static void Main(string[] args) {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
