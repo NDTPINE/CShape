8c5931e0c415e4c7ad0145e95157d49cbe9faf6d
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-05-13 12:36:11 +0700

    Update Climbing the Leaderboard.cs
    TIME OUT TEST CASE.

--- Climbing the Leaderboard/Climbing the Leaderboard.cs
+++ Climbing the Leaderboard/Climbing the Leaderboard.cs
@@ -16,7 +16,39 @@ class Solution {
 
     // Complete the climbingLeaderboard function below.
     static int[] climbingLeaderboard(int[] scores, int[] alice) {
-
+        int[] result = new int[alice.Length];
+        int index = scores[0];
+        int count = 0;
+        for (int i = 1; i < scores.Length; i++){
+            if (scores[i] == index) {
+                scores[i] = 0;
+                count++;
+            } else index = scores[i];
+        }
+        Array.Sort(scores);
+        Array.Reverse(scores);
+        Array.Resize(ref scores, scores.Length - count);
+    
+        index = 0;
+        for (int m = 0; m < alice.Length; m++){
+            if (alice[m] < scores[scores.Length - 1]){
+                result[index] = scores.Length +1;
+                index++;
+            } else if ( alice[m] > scores[0]){
+                result[index] = 1;
+                index++;
+            }else {
+                for(int j = 0; j < scores.Length; j++){
+                    if (alice[m] >= scores[j]) {
+                        result[index] = j+1;
+                        index++;
+                        break;
+                    }
+                }
+            }
+  
+        }
+        return result;
 
     }
 
