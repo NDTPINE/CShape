eb7ce14a26da8972502f1c7039e52178827c373c
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-05-13 09:58:34 +0700

    Create Forming a Magic Square.cs
    Creat the problems

---
+++ Forming a Magic Square/Forming a Magic Square.cs
@@ -0,0 +1,39 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the formingMagicSquare function below.
+    static int formingMagicSquare(int[][] s) {
+
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[][] s = new int[3][];
+
+        for (int i = 0; i < 3; i++) {
+            s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
+        }
+
+        int result = formingMagicSquare(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
