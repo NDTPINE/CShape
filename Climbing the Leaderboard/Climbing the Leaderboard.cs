e15f62eb38c8d26072490fd9970a2a919bcef8fd
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-05-14 11:42:40 +0700

    Update Climbing the Leaderboard.cs
    update

--- Climbing the Leaderboard/Climbing the Leaderboard.cs
+++ Climbing the Leaderboard/Climbing the Leaderboard.cs
@@ -17,39 +17,33 @@ class Solution {
     // Complete the climbingLeaderboard function below.
     static int[] climbingLeaderboard(int[] scores, int[] alice) {
         int[] result = new int[alice.Length];
-        for (int m = 0; m < alice.Length; m++){
-            result[m] = rank2(scores,alice[m]);
+        int[] rank = new int[scores.Length];
+        rank[0] = 1;
+        for (int i = 1; i< scores.Length; i++){
+            if (scores[i] == scores[i-1]){
+                rank[i] = rank[i-1];
+            } else rank[i] = rank[i-1] + 1;
+        }
+        int star = 0;
+        int end = scores.Length -1;
+        for (int j = 0;j < alice.Length;j++){
+            int temp = binarySearch(scores, rank, star,end,alice[j]);
+            result[j] = temp;
         }
         return result;
     }
-    public static int rank2(int[] scores, int number){
-        if (number > scores[0]) return 1;
-        int count = 0;
-        int temp2 = scores[0];
-        for (int j = 1; j < scores.Length; j++){
-            if (scores[j] == temp2) {
-                count++;
-            } else temp2 = scores[j];
-        }
-        if (number < scores[scores.Length - 1]) return scores.Length - count +1;
-        Array.Resize(ref scores, scores.Length + 1);
-        scores[scores.Length - 1] = number;
-        Array.Sort(scores);
-        Array.Reverse(scores);
-        int[] rank = new int[scores.Length];
-        int temp = scores[0];
-        int pointrank = 1;
-        for (int i = 0; i < scores.Length;i++){
-            if (scores[i] == temp) rank[i] = pointrank;
-            else {
-                pointrank++;
-                temp = scores[i];
-                rank[i] = pointrank;
-            }
-        }
-        return rank[Array.IndexOf(scores,number)];
+    public static int binarySearch(int[] score, int[] rank,int star, int end, int news){
+        int mid = (star + end)/2;
+        if (star == end){
+            if (news >= score[star]) return rank[star];
+            else return rank[star] + 1;
+        } else if (news >score[mid]){
+            return binarySearch(score, rank, star, Math.Max(mid -1, 0), news);
+        } else if(news < score[mid]) {
+            return binarySearch(score, rank, Math.Min(mid+1, score.Length - 1), end, news);
+        } else return rank[mid];
     }
-
+    
     static void Main(string[] args) {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
