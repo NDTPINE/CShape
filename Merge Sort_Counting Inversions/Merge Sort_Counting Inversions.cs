7ac35ce9ee380b941425ecbe627ce0987faecf9e
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-05-15 17:43:28 +0700

    Update Merge Sort_Counting Inversions.cs
    the problems is timeout.

--- Merge Sort_Counting Inversions/Merge Sort_Counting Inversions.cs
+++ Merge Sort_Counting Inversions/Merge Sort_Counting Inversions.cs
@@ -1,41 +1,54 @@
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+    using System.Text.RegularExpressions;
+    using System.Text;
+    using System;
 
-class Solution {
+    class Solution {
 
-    // Complete the countInversions function below.
-    static long countInversions(int[] arr) {
-
-
-    }
-
-    static void Main(string[] args) {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int t = Convert.ToInt32(Console.ReadLine());
-
-        for (int tItr = 0; tItr < t; tItr++) {
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-            ;
-            long result = countInversions(arr);
-
-            textWriter.WriteLine(result);
+        // Complete the countInversions function below.
+        static long countInversions(int[] arr) {
+            long count = 0;
+            for (int i = 0; i < arr.Length -1; i++){
+                for ( int j = i + 1; j < arr.Length; j++){
+                    if (arr[j] < arr[i]){
+                        swap(ref arr[j], ref arr[i]);
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        static void swap (ref int a, ref int b){
+            int temp = a;
+            a = b;
+            b = temp;
         }
 
-        textWriter.Flush();
-        textWriter.Close();
+        static void Main(string[] args) {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++) {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+                ;
+                long result = countInversions(arr);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
     }
-}
