5e8076a2a3faea758559261baa8abeac0ade2505
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-05-21 13:31:35 +0700

    Update Common Child.cs
    trans about array 2 way to prosovle the problems.

--- Common Child/Common Child.cs
+++ Common Child/Common Child.cs
@@ -17,39 +17,33 @@ class Solution {
     // Complete the commonChild function below.
     static int commonChild(string s1, string s2) {
         int len = 0;
-        int pos1;
-        int pos2;
-        string str = "";
-        while (s1.Length != 0 && s2.Length != 0){
-            pos1 = s1.IndexOf(s2[0]);
-            pos2 = s2.IndexOf(s1[0]);
-            if (pos1 == -1){
-                s2 = s2.Substring(1);
-            } else if (pos2 == -1){
-                s1 = s1.Substring(1);
-            } else if (s1[0] == s2[0]){
-                str = String.Concat(str, Convert.ToString(s1[0]));
-                s1 = s1.Substring(1);
-                s2 = s2.Substring(1);
-            } else {
-                if (pos1 > pos2 && s1.IndexOf(s2[1]) < pos1){
-                    s1 = s1.Substring(1);
-                } else if (pos1 < pos2 && s2.IndexOf(s1[1])< pos2){
-                    s2 = s2.Substring(1);
-                } else if (pos1 < pos2) {
-                    s1 = s1.Substring(pos1);
-                    str = String.Concat(str,Convert.ToString(s2[0]));
-                    s2 = s2.Substring(1);
-                } else if (pos1 > pos2){
-                    s2 = s2.Substring(pos2);
-                    str = String.Concat(str,Convert.ToString(s1[0]));
-                    s1 = s1.Substring(1);
+        s1 = String.Concat(" ", s1.Substring(0));
+        s2 = String.Concat(" ", s2.Substring(0));
+        int[,] arr = new int[s1.Length,s2.Length];
+        int start = 1;
+        int len1 = s1.Length -1;
+        int len2 = s2.Length -1;
+        while (start <= len1 && start <= len2 && s1[start] == s2[start]){
+            start++;
+            len++;
+        }
+        while (start <= len1 && start <= len2 && s1[len1] == s2[len2]){
+            len1--;
+            len2--;
+            len++;
+        }
+        
+        for (int i = start; i <= len1; i++){
+            for (int j = start; j <= len2; j++){
+                if (s1[i] == s2[j]){
+                    arr[i,j] = arr[i -1, j -1] +1;
+                } else {
+                    arr[i,j] = Math.Max(arr[i, j -1],arr[i -1,j]);
                 }
             }
         }
-        len = str.Length;
+        len += arr[len1, len2];
         return len;
-
     }
 
     static void Main(string[] args) {
