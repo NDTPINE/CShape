fa057026e7e11c511f6eacd78c2da273a684e22d
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-05-24 15:33:21 +0700

    Update Frequency Queries.cs
    Timeout test case 11.

--- Frequency Queries/Frequency Queries.cs
+++ Frequency Queries/Frequency Queries.cs
@@ -15,22 +15,25 @@ using System;
 class Solution {
 
     // Complete the freqQuery function below.
-    static List<int> freqQuery(List<List<int>> queries) {
+    static List<int> freqQuery(int[,] queries) {
     List<int> result = new List<int>();
     Dictionary<int, int> temp = new Dictionary<int, int>();
-    for (int i = 0; i < queries.Count; i++){
-        if (queries[i][0] == 1){
-            if (temp.ContainsKey(queries[i][1])){
-                temp[queries[i][1]]++;
-            } else temp[queries[i][1]] = 1;
-        }
-        if (queries[i][0] == 2){
-            if (temp.ContainsKey(queries[i][1])){
-                temp[queries[i][1]]--;
-            } //else temp[queries[i][1]] = -1;
-        }
-        if (queries[i][0] == 3){
-            if (temp.ContainsValue(queries[i][1])){
+    for (int i = 0; i < queries.GetLength(0); i++){
+        if (queries[i,0] == 1){
+            if (temp.ContainsKey(queries[i,1])){
+                temp[queries[i,1]]++;
+            } else temp[queries[i,1]] = 1;
+        } else  
+        if (queries[i,0] == 2){
+            if (temp.ContainsKey(queries[i,1])){
+                temp[queries[i,1]]--;
+            } else temp[queries[i,1]] = 0;
+            if (temp[queries[i,1]] <= 0){
+                temp.Remove(queries[i,1]);
+            }
+        } else
+        if (queries[i,0] == 3 ){
+            if (temp.ContainsValue(queries[i,1])){
                 result.Add(1);
             } else result.Add(0);
         }
@@ -44,15 +47,18 @@ class Solution {
 
         int q = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<List<int>> queries = new List<List<int>>();
+                var queries = new int[q,2];
 
-        for (int i = 0; i < q; i++) {
-            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
-        }
+                for (int i = 0; i < q; i++)
+                {
+                    var nq = new int[2];
+                    nq = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToArray();
+                    queries[i, 0] = nq[0];
+                    queries[i, 1] = nq[1];
+                }
 
-        List<int> ans = freqQuery(queries);
-
-        textWriter.WriteLine(String.Join("\n", ans));
+                List<int> ans = freqQuery(queries);
+                textWriter.WriteLine(String.Join("\n", ans));
 
         textWriter.Flush();
         textWriter.Close();
